c947cfe2560de0b41edb9100acfdffa04c93fee6
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2019-03-12 22:45:01 +0100

    fixed env settings

--- build.py
+++ build.py
@@ -8,8 +8,8 @@ os.environ["CONAN_USERNAME"]                = "qno"
 os.environ["CONAN_PASSWORD"]                = os.environ["BINTRAY_API_KEY"]
 os.environ["CONAN_LOGIN_USERNAME"]          = os.environ["BINTRAY_LOGIN"]
 os.environ["CONAN_STABLE_BRANCH_PATTERN"]   = "stable/*"
-os.environ["CONAN_UPLOAD_ONLY_WHEN_STABLE"] = False
-os.environ["CONAN_DOCKER_32_IMAGES"]        = True
+os.environ["CONAN_UPLOAD_ONLY_WHEN_STABLE"] = "0"
+os.environ["CONAN_DOCKER_32_IMAGES"]        = "1"
 
 
 def _is_static_msvc_build(build):
