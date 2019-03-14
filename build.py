4bce3d89a6a34f64a54de8d08af81e9d42a557de
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2019-03-14 22:06:22 +0100

    try fix upload problems

--- build.py
+++ build.py
@@ -1,15 +1,17 @@
 from bincrafters import build_template_default
 import os
 
-# see https://github.com/bincrafters/bincrafters-package-tools
-os.environ["BINTRAY_REPOSITORY"]            = "conan-public"
+# see https://github.com/bincrafters/bincrafters-package-tools/blob/master/README.md
+# see https://github.com/conan-io/conan-package-tools/blob/develop/README.md
+#os.environ["BINTRAY_REPOSITORY"]            = "conan-public"
 os.environ["CONAN_UPLOAD"]                  = "https://api.bintray.com/conan/qno/conan-public"
 os.environ["CONAN_USERNAME"]                = "qno"
 os.environ["CONAN_PASSWORD"]                = os.environ["BINTRAY_API_KEY"]
-os.environ["CONAN_LOGIN_USERNAME"]          = os.environ["BINTRAY_LOGIN"]
+os.environ["CONAN_LOGIN_USERNAME"]          = "qno"
 os.environ["CONAN_STABLE_BRANCH_PATTERN"]   = "stable/*"
 os.environ["CONAN_UPLOAD_ONLY_WHEN_STABLE"] = "0"
 os.environ["CONAN_DOCKER_32_IMAGES"]        = "1"
+os.environ["CONAN_CHANNEL"]        = "testing"
 
 
 def _is_static_msvc_build(build):
